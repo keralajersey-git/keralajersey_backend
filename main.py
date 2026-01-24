545156bdc19324d75b7539665f32571d6e1cd781
Risheb.s <rishebs123456@gmail.com>
2026-01-24 20:03:36 +0530

    added category

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Kerala Jersey Backend")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://keralajersey.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
